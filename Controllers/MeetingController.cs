95845d780b9b745abe9bd61c7187e14302881441
Fatih Altunel <jjrousseau1128@gmail.com>
2024-04-25 14:46:44 +0300

    MVC architecture is formed

---
+++ Controllers/MeetingController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingApp.Controllers
+{
+    public class MeetingController : Controller{
+        public IActionResult Index(){
+            return View();  
+        }  
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews(); //MVC şablonu oluşturmak için gerekli servis
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+//{controller}/{action}/{id?} şemasını ekler
+//app.MapDefaultControllerRoute();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
