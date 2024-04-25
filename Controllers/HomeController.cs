e1a572fa6457785053eff6cc2bb0c83c8226bceb
Fatih Altunel <jjrousseau1128@gmail.com>
2024-04-25 16:54:18 +0300

    Participation form designed

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,19 @@
+using MeetingApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MeetingApp.Controllers
 {
     public class HomeController : Controller{
         public IActionResult Index(){
-            return View();
+
+            var meetingInfo = new MeetingInfo(){
+                Id=1,
+                Location="Istanbul ABC Kongre Merkezi",
+                Date=new DateTime(2024,01,20,20,0,0),
+                NumberOfPeople=93,
+            };
+
+            return View(meetingInfo);
         }
     }
 }
